ea4e155896d34cf36445522fb1ed77d179132802
Andre Wohnsland <Andre_Wohnsland@web.de>
2019-07-29 11:55:19 +0200

    Added the display function of the leaderboard

--- src/setup_plotdialog.py
+++ src/setup_plotdialog.py
@@ -1,6 +1,12 @@
 import sys
 import sqlite3
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -26,10 +32,6 @@ class PlotDialog(QDialog, Ui_PlotDialog):
         self.PB_plot_all.clicked.connect(lambda: self.plot_clicked(active=False))
         self.PB_back.clicked.connect(self.back_clicked)
 
-    def back_clicked(self):
-        """ Closes the window without any further action. """
-        self.close()
-
     def plot_clicked(self, active=True):
         """ Plot a leaderboard of the best x employees. By best is ment the most entries. 
         Differentiates between all DB entries, and only still active employees.
@@ -37,8 +39,10 @@ class PlotDialog(QDialog, Ui_PlotDialog):
         # Selects the 5 employes with the highest comsumption either from all ore active
         if active:
             bonusstring = " WHERE enabled=1"
+            headerstring="(active)"
         else:
             bonusstring = ""
+            headerstring="(all time)"
         employee_data = self.ms.c.execute("SELECT first_name, last_name, amount FROM employees{} ORDER BY amount DESC".format(bonusstring))
         namelist = []
         amountlist =  []
@@ -47,9 +51,64 @@ class PlotDialog(QDialog, Ui_PlotDialog):
             if number == 5:
                 break
             last_name = row[1]
-            namelist.append("{} {}.".format(row[0], last_name[0]))
+            namelist.append("      {} {}.".format(row[0], last_name[0]))
             amountlist.append(row[2])
         print(20*"-")
         for name, amount in zip(namelist, amountlist):
             print(name, amount)
+        self.graphwindow = GraphWindow(self, plotvalues=amountlist, plotlabels=namelist, headerstring=headerstring)
+        self.graphwindow.show()
+        # y_pos = np.arange(len(namelist))
+        # plt.barh(y_pos, amountlist, align='center')
+        # plt.yticks(y_pos, namelist)
+        # plt.show()
 
+    def back_clicked(self):
+        """ Closes the window without any further action. """
+        self.close()
+
+
+class GraphWindow(QDialog):
+    """ A Popup window to show a graph of the user with the most quantities. """
+    def __init__(self, parent, plotvalues=None, plotlabels=None, headerstring="all time"):
+        super(GraphWindow, self).__init__(parent)
+        self.resize(480, 320)
+        self.setMinimumSize(QSize(480, 320))
+        self.setMaximumSize(QSize(480, 320))
+        self.setWindowTitle("Leaderboard {}".format(headerstring))
+        # a figure instance to plot on
+        self.figure = plt.figure()
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+        # set the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+        # clears the old values and then adds a subplot to isert all the data
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        # plots a horizontal bargraph with the values and the namelabels
+        y_pos = np.arange(len(plotlabels))
+        ax.xaxis.grid(linestyle='--', color='w', zorder=0)
+        ax.barh(y_pos, plotvalues, tick_label=plotlabels, zorder=3)
+        # gets the numbers in front of each bar
+        for i, v in enumerate(plotvalues):
+            ax.text(max(plotvalues)/50, i-0.05, str(v), color='w', fontweight='bold', va='center', fontsize=14)
+        # removes the ticks from each axis
+        for ticx in ax.xaxis.get_major_ticks():
+            ticx.tick1On = ticx.tick2On = False
+        for ticy in ax.yaxis.get_major_ticks():
+            ticy.tick1On = ticy.tick2On = False
+        # recolours the style into invers (black is white, white is black)
+        self.figure.patch.set_facecolor('k')
+        ax.patch.set_facecolor('k')
+        ax.spines['bottom'].set_color('w')
+        ax.spines['top'].set_color('w')
+        ax.spines['left'].set_color('w')
+        ax.spines['right'].set_color('w')
+        plt.setp(ax.get_xticklabels(), color="w", fontsize=12)
+        plt.setp(ax.get_yticklabels(), color="w", fontsize=12)
+        plt.tight_layout()
+        # refresh canvas
+        self.canvas.draw()
