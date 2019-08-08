35afbd21d5819d81979acd96bed8821afacaa3b9
Wohnsland <Andre.Wohnsland@thi.de>
2019-08-08 07:59:22 +0200

    Initial commit

---
+++ desctop_ui/desctopapp_src.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import configparser
+
+from PyQt5.QtWidgets import QApplication
+
+from desctop_ui.desctopapp_src import MainScreen
+
+
+db_name = "employees_dummy"    
+paymentcall_threshold = 10      # When the pay message got displayed (critical is 1.5 that value)
+quantcosts = 0.25               # cost of one quant (can also be 0)
+quantname = "coffee"            # name of your quants
+
+# path gerneration for the DB
+subfoldername = "data"
+dirpath = os.path.dirname(__file__)
+db_path = os.path.join(dirpath, subfoldername, "{}.db".format(db_name))
+
+if __name__ == "__main__":
+    # creates the application
+    app = QApplication(sys.argv)
+    #creates the mainscreen, sets it to fixed size and fullscreen
+    w = MainScreen(db_path=db_path)
+    w.show()
+    # runs the app until the exit
+    sys.exit(app.exec_())
